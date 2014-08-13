997043ce19cd8ab1c16c9d4e8caf0b21ec638472
CaoPhiHung <caophihung8392@gmail.com>
2014-08-13 12:33:41 +0700

    check file exist
    check if the file is exist no need to connect to the website just read
    from the file then add to the data

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+string data;
+
 // function return how many size the program handled
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
@@ -13,29 +15,46 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *use
 }
 
 int main(void)
-{
-	CURL *curl;
-	CURLcode result; // store result of of CURL's webpage retrieval => error checking
-	string source;
-	string data;
-	curl = curl_easy_init();
-	curl_easy_setopt(curl, CURLOPT_URL, "http://www.pathofexile.com/passive-skill-tree");
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &source);
-    result = curl_easy_perform(curl);
-    curl_easy_cleanup(curl);
-
-    size_t start = source.find("var passiveSkillTreeData = ");
-    size_t end = source.find("0.3835]}");
-   	data = source.substr(start+27,end-start - 19);
-
-    //write to file
-	ofstream myfile;
-  	myfile.open ("passive_skill_tree.txt");
-  	myfile << data;
-  	myfile.close();
+{	
+	//check if the file exist => read from the file => add to data
+	if(fstream("passive_skill_tree.txt"))
+	{
+		//read from file => add to data
+		ifstream myfile ("passive_skill_tree.txt");
+  		if (myfile.is_open())
+  		{
+    		while ( getline (myfile,data) )
+    		{
+      			//cout << data << '\n';
+    		}
+    		myfile.close();
+  		}else{ cout << "Unable to open file"; }
 
 
+
+	}else{
+		//if file not exist => connect to website and download infor
+		CURL *curl;
+		CURLcode result; // store result of of CURL's webpage retrieval => error checking
+		string source;
+		curl = curl_easy_init();
+		curl_easy_setopt(curl, CURLOPT_URL, "http://www.pathofexile.com/passive-skill-tree");
+	    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+	    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &source);
+	    result = curl_easy_perform(curl);
+	    curl_easy_cleanup(curl);
+
+	    size_t start = source.find("var passiveSkillTreeData = ");
+	    size_t end = source.find("0.3835]}");
+	   	data = source.substr(start+27,end-start - 19);
+		
+	    //write to file
+		ofstream myfile;
+	  	myfile.open ("passive_skill_tree.txt");
+	  	myfile << data;
+	  	myfile.close();
+
+	}
 
   return 0;
 }
