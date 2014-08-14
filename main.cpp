b6c45852115c61a0d6240b015a9685c04f2824d2
CaoPhiHung <caophihung8392@gmail.com>
2014-08-14 11:05:23 +0700

    change the main

--- main.cpp
+++ main.cpp
@@ -18,55 +18,64 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *use
     return size * nmemb;
 }
 
+void connectToWebsite()
+{
+	CURL *curl;
+	CURLcode result; // store result of of CURL's webpage retrieval => error checking
+	string source;
+	curl = curl_easy_init();
+	curl_easy_setopt(curl, CURLOPT_URL, "http://www.pathofexile.com/passive-skill-tree");
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &source);
+	result = curl_easy_perform(curl);
+	curl_easy_cleanup(curl);
+
+	size_t start = source.find("var passiveSkillTreeData = ");
+	size_t end = source.find("0.3835]}");
+	data = source.substr(start+27,end-start - 19);
+	
+	//to make the program run faster we just need to connect to the website 1 time then we write the ritrival webpage to text file
+	//then next time run the program we just need read from the text file => much faster
+	
+	//write to file
+	ofstream myfile;
+	myfile.open ("passive_skill_tree.txt");
+	myfile << data;
+	myfile.close();
+}
+
+void readFromTextFile()
+{
+	//read from file => add to data
+	ifstream myfile ("passive_skill_tree.txt");
+  	if (myfile.is_open())
+  	{
+    	while ( getline (myfile,data) )
+    	{
+      		//cout << data << '\n';
+    	}
+    	myfile.close();
+  	}else{ cout << "Unable to open file"; }
+
+	Document document;
+	if ( document.Parse<0>( data.c_str() ).HasParseError() ) {
+    	cout << "Error parsing" << std::endl;
+	} else {
+    	const Value& a = document["nodes"];
+    	assert(a.IsArray());
+    	a[SizeType(0)];
+	}
+}
+
 int main(void)
 {	
 	//check if the file exist => read from the file => add to data
 	if(fstream("passive_skill_tree.txt"))
 	{
-		//read from file => add to data
-		ifstream myfile ("passive_skill_tree.txt");
-  		if (myfile.is_open())
-  		{
-    		while ( getline (myfile,data) )
-    		{
-      			//cout << data << '\n';
-    		}
-    		myfile.close();
-  		}else{ cout << "Unable to open file"; }
-
-  		Document document;
-		if ( document.Parse<0>( data.c_str() ).HasParseError() ) {
-    		std::cout << "Error parsing" << std::endl;
-		} else {
-    		if ( document[ "characterData" ].IsObject() ) {
-        		rapidjson::StringBuffer sb;
-        		rapidjson::Writer<rapidjson::StringBuffer> writer( sb );
-        		document[ "characterData" ].Accept( writer );
-        		std::cout << sb.GetString() << std::endl;
-    		}
-		}
+		readFromTextFile();
 	}else{
 		//if file not exist => connect to website and download infor
-		CURL *curl;
-		CURLcode result; // store result of of CURL's webpage retrieval => error checking
-		string source;
-		curl = curl_easy_init();
-		curl_easy_setopt(curl, CURLOPT_URL, "http://www.pathofexile.com/passive-skill-tree");
-	    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-	    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &source);
-	    result = curl_easy_perform(curl);
-	    curl_easy_cleanup(curl);
-
-	    size_t start = source.find("var passiveSkillTreeData = ");
-	    size_t end = source.find("0.3835]}");
-	   	data = source.substr(start+27,end-start - 19);
-		
-	    //write to file
-		ofstream myfile;
-	  	myfile.open ("passive_skill_tree.txt");
-	  	myfile << data;
-	  	myfile.close();
-
+		connectToWebsite();
 	}
 
   return 0;
